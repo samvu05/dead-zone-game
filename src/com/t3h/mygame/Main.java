e3555db29e447fdda735681e2faea75396df098d
Dinh Sam Vu <63152436+samvu05@users.noreply.github.com>
2020-04-06 21:34:41 +0700

    Update Main.java

--- src/com/t3h/mygame/Main.java
+++ src/com/t3h/mygame/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         GUI gui = new GUI();
         gui.setVisible(true);
+        System.out.println("test Git");
 
         //Android Studio
         //SDK
